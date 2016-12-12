4d55301d408737ef574b3d548e4cdafc1275487d
Isaac <Isaac234517@gmail.com>
2016-12-12 23:00:44 +0800

    add react-route and svg loader dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -10,8 +10,9 @@ module.exports ={
 		loaders:[
 		 {  test: /\.jsx?$/, exclude: /node_modules/, loader: 'babel-loader'},
 		 { test: /\.css$/, loader: "style-loader!css-loader" },
-      	 { test: /\.(png|jpg)$/, loader: "url-loader?limit=100000" },
-         { test: /\.jpg$/, loader: "file-loader" }
+      	 { test: /\.(png|jpg|svg)$/, loader: "url-loader?limit=100000" },
+         { test: /\.jpg$/, loader: "file-loader" },
+         { test: /\.svg$/, loader: "svg-url-loader"}
 		]
 	},
 	devServer:{
